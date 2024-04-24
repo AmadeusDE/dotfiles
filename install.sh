a92e6e4aaba2da7af7295f0fcba4a0d50a7dcd97
Dakota <100294596+unix-supremacist@users.noreply.github.com>
2024-04-23 21:30:04 -0500

    cleanup packages, install conky, try to do pipewire(not completely working)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Syu $(cat packages)
+sudo pacman -Syu $(cat packages | tr "\n" " ")
 sudo chown root "$(pwd)"/doas.conf
 sudo ln -sf "$(pwd)"/doas.conf /etc/doas.conf
 doas pacman -Rns sudo
@@ -12,4 +12,5 @@ ln -sf "$(pwd)"/vars.conf "$HOME"/.dotfiles/vars.conf
 ln -sf "$(pwd)"/hyprland.conf "$HOME"/.config/hypr/hyprland.conf
 ln -sf "$(pwd)"/hyprlock.conf "$HOME"/.config/hypr/hyprlock.conf
 ln -sf "$(pwd)"/kitty.conf "$HOME"/.config/kitty/kitty.conf
+ln -sf "$(pwd)"/conky.conf "$HOME"/.config/conky/conky.conf
 doas ln -sf "$(pwd)"/sddm.conf /etc/sddm.conf
