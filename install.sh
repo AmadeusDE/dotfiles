7f2b10e2eb442bfac01ec154f62041bd11e7b997
Dakota <100294596+unix-supremacist@users.noreply.github.com>
2024-04-27 19:28:40 -0500

    finally do shell related stuff, refactor install script.

--- install.sh
+++ install.sh
@@ -1,17 +1,37 @@
 #!/bin/sh
 
-sudo pacman -Syu $(cat packages | tr "\n" " ")
-sudo chown root "$(pwd)"/doas.conf
-sudo ln -sf "$(pwd)"/doas.conf /etc/doas.conf
+#intentional splitting
+# shellcheck disable=SC2046
+sudo pacman -Syu $(tr "\n" " " < packages)
+shellcheck "$(realpath "$0")"
+sudo chown root "$(realpath doas.conf)"
+sudo ln -sf "$(realpath doas.conf)" /etc/doas.conf
+yay -Syu $(tr "\n" " " < aurpackages)
 doas pacman -Rns sudo
 doas pacman -Syu doas-sudo-shim
-ln -sf "$(pwd)"/wallpaper.sh "$HOME"/.dotfiles/wallpaper.sh
-ln -sf "$(pwd)"/autostart.conf "$HOME"/.dotfiles/autostart.conf
-ln -sf "$(pwd)"/hypridle.conf "$HOME"/.dotfiles/hypridle.conf
-ln -sf "$(pwd)"/vars.conf "$HOME"/.dotfiles/vars.conf
-ln -sf "$(pwd)"/hyprland.conf "$HOME"/.config/hypr/hyprland.conf
-ln -sf "$(pwd)"/hyprlock.conf "$HOME"/.config/hypr/hyprlock.conf
-ln -sf "$(pwd)"/kitty.conf "$HOME"/.config/kitty/kitty.conf
-ln -sf "$(pwd)"/conky.conf "$HOME"/.config/conky/conky.conf
+doas usermod --shell /bin/zsh "$USER"
+echo "After this you'll be asked what version to downgrade bluez too, if you want bluetooth to work use 5.68, press any key to continue"
+#intentional dummy and -r doesn't matter because the var is never used
+# shellcheck disable=SC2034,SC2162
+read dummy
+doas downgrade bluez bluez-libs
+ln -sf "$(realpath wallpaper.sh)" "$HOME"/.dotfiles/wallpaper.sh
+ln -sf "$(realpath autostart.conf)" "$HOME"/.dotfiles/autostart.conf
+ln -sf "$(realpath hypridle.conf)" "$HOME"/.dotfiles/hypridle.conf
+ln -sf "$(realpath vars.conf)" "$HOME"/.dotfiles/vars.conf
+ln -sf "$(realpath hyprland.conf)" "$HOME"/.config/hypr/hyprland.conf
+ln -sf "$(realpath hyprlock.conf)" "$HOME"/.config/hypr/hyprlock.conf
+ln -sf "$(realpath kitty.conf)" "$HOME"/.config/kitty/kitty.conf
+ln -sf "$(realpath conky.conf)" "$HOME"/.config/conky/conky.conf
+ln -sf "$(realpath .zshrc)" "$HOME"/.zshrc
 doas ln -sf "$(pwd)"/sddm.conf /etc/sddm.conf
 cp /usr/share/pipewire/pipewire.conf .config/pipewire/pipewire.conf
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl https://raw.githubusercontent.com/sbugzu/gruvbox-zsh/ed9693db2f8ab064b70eb8932d067c1e0bab4a85/gruvbox.zsh-theme > ~/.oh-my-zsh/custom/themes/gruvbox.zsh-theme
+# you can't double quote these
+# shellcheck disable=SC2086
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# shellcheck disable=SC2086
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# shellcheck disable=SC2086
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
