501697841cafeebe9b76eff3b252cea6aa477c71
unix-supremacist <100294596+unix-supremacist@users.noreply.github.com>
2025-06-11 00:36:21 -0500

    generic additions, cleanup, bugfixes, kdeconnect autostart

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #WALLPAPER=$(find ~/Pictures/Wallpaper -type f | shuf -n 1)
+pgrep mpvpaper | xargs kill
 mpvpaper -o "input-ipc-server=/tmp/mpv-socket no-audio --loop-playlist" ALL ~/sute-girl-minecraft.1920x1080.mp4&
